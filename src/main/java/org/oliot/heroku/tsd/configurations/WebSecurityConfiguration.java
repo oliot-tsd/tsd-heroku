a280ebfe47e8d29803b05318847321f6bee26550
Gaurav Kalra <gvkalra@kaist.ac.kr>
2017-07-27 19:50:34 +0900

    require secure channel

--- src/main/java/org/oliot/heroku/tsd/configurations/WebSecurityConfiguration.java
+++ src/main/java/org/oliot/heroku/tsd/configurations/WebSecurityConfiguration.java
@@ -29,6 +29,9 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .requiresChannel()
+                    .anyRequest().requiresSecure()
+                .and()
                 .authorizeRequests()
                     .antMatchers(HttpMethod.GET, "/product/**").permitAll()
                 .and()
