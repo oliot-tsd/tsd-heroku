9aea4fe17bff5ba58f633eaf68c949a6b80b32f8
Gaurav Kalra <gvkalra@kaist.ac.kr>
2017-07-27 20:15:16 +0900

    basic login

--- src/main/java/org/oliot/heroku/tsd/configurations/WebSecurityConfiguration.java
+++ src/main/java/org/oliot/heroku/tsd/configurations/WebSecurityConfiguration.java
@@ -18,7 +18,9 @@ package org.oliot.heroku.tsd.configurations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -35,7 +37,18 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                     .antMatchers(HttpMethod.GET, "/product/**").permitAll()
                 .and()
+                .formLogin()
+                .and()
                 .authorizeRequests()
                     .anyRequest().hasRole("ADMIN");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                    .withUser(System.getenv("ADMIN_USERNAME"))
+                    .password(System.getenv("ADMIN_PASSWORD"))
+                    .roles("ADMIN");
     }
 }
